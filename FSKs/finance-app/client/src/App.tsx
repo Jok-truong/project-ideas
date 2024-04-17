56ca43f30290458b508b028768e55d329bfa9451
Huy Truong <o1lkey222k@gmail.com>
2024-04-17 23:45:10 +0700

    finance-app: mui configurations and themes - FE

--- FSKs/finance-app/client/src/App.tsx
+++ FSKs/finance-app/client/src/App.tsx
@@ -1,5 +1,17 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+
 function App() {
-  return <>Setup Client</>;
+  const theme = useMemo(() => createTheme(), []);
+
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        Setup Client
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
