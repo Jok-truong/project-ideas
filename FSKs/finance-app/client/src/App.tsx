43d2352e7d0c77395d3fbd3ab450cf3eeb02fe92
Huy Truong <o1lkey222k@gmail.com>
2024-04-18 00:07:55 +0700

    finance-app: react router and navigateion - FE

--- FSKs/finance-app/client/src/App.tsx
+++ FSKs/finance-app/client/src/App.tsx
@@ -1,15 +1,27 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Predictions from "./pages/Predictions";
+import Navbar from "./components/Navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(), []);
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        Setup Client
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
