e1bb4773c42db32cec7b3c3f79994cf9022597ab
Huy Truong <o1lkey222k@gmail.com>
2024-04-18 16:52:47 +0700

    finance-app: connected to mongodb

--- FSKs/finance-app/server/src/index.ts
+++ FSKs/finance-app/server/src/index.ts
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wnqawtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+  .connect(MONGODB_URI, {
+    autoIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .then(() => main())
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+  });
 
 const main = async () => {
   const app = express();
@@ -12,5 +26,3 @@ const main = async () => {
 
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 };
-
-main().catch((error) => console.log(error));
