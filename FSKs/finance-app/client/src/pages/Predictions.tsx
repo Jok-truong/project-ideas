7c32ca05e1c6ac360c3dc489dc9fe1fc55a9ecb9
Huy Truong <o1lkey222k@gmail.com>
2024-04-18 22:52:27 +0700

    finance-app: predictions and ML

--- FSKs/finance-app/client/src/pages/Predictions.tsx
+++ FSKs/finance-app/client/src/pages/Predictions.tsx
@@ -1,5 +1,122 @@
+import { useMemo, useState } from "react";
+import { useGetKpisQuery } from "../state/api";
+import RowBox from "../style-components/RowBox";
+import FlexBetween from "../style-components/FlexBetween";
+import { Box, Button, Typography } from "@mui/material";
+import {
+  CartesianGrid,
+  Label,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import regression, { DataPoint } from "regression";
+
 function Predictions() {
-  return <div>Predictions</div>;
+  const [isPredictions, setIsPredictions] = useState(false);
+  const { data: kpiData } = useGetKpisQuery();
+
+  const formattedData = useMemo(() => {
+    if (!kpiData) return [];
+    const monthData = kpiData[0].monthlyData;
+
+    const formatted: DataPoint[] = monthData.map(({ revenue }, i: number) => {
+      return [i, revenue];
+    });
+
+    // Fits the input data to a straight line with the equation y = mx + c
+    const regressionLine = regression.linear(formatted);
+
+    return monthData.map(({ month, revenue }, i: number) => {
+      return {
+        name: month,
+        "Actual Revenue": revenue,
+        "Regression Line": regressionLine.points[i][1],
+        "Predicted Revenue": regressionLine.predict(i + 12)[1],
+      };
+    });
+  }, [kpiData]);
+
+  return (
+    <RowBox width="100%" height="100%" p="1rem" overflow="hidden">
+      <FlexBetween m="1rem 2.5rem" gap="1rem">
+        <Box>
+          <Typography variant="h3" color={"#f2b455"}>
+            Revenue and Predictions
+          </Typography>
+          <Typography variant="h6" color={"#c2c5ce"}>
+            charted revenue and predicted revenue based on a simple linear
+            regression model
+          </Typography>
+        </Box>
+        <Button
+          onClick={() => setIsPredictions(!isPredictions)}
+          sx={{
+            color: "#242427",
+            backgroundColor: "#f2b455",
+            boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,.4)",
+          }}
+        >
+          Show Predicted Revenue for Next Year
+        </Button>
+      </FlexBetween>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={formattedData}
+          margin={{
+            top: 20,
+            right: 75,
+            left: 20,
+            bottom: 80,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke={"#48494e"} />
+          <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }}>
+            <Label value="Month" offset={-5} position="insideBottom" />
+          </XAxis>
+          <YAxis
+            domain={[12000, 26000]}
+            axisLine={{ strokeWidth: "0" }}
+            style={{ fontSize: "10px" }}
+            tickFormatter={(v) => `$${v}`}
+          >
+            <Label
+              value="Revenue in USD"
+              angle={-90}
+              offset={-5}
+              position="insideLeft"
+            />
+          </YAxis>
+          <Tooltip />
+          <Legend verticalAlign="top" />
+          <Line
+            type="monotone"
+            dataKey="Actual Revenue"
+            stroke={"#12efc8"}
+            strokeWidth={0}
+            dot={{ strokeWidth: 5 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="Regression Line"
+            stroke="#8884d8"
+            dot={false}
+          />
+          {isPredictions && (
+            <Line
+              strokeDasharray="5 5"
+              dataKey="Predicted Revenue"
+              stroke={"#f2b455"}
+            />
+          )}
+        </LineChart>
+      </ResponsiveContainer>
+    </RowBox>
+  );
 }
 
 export default Predictions;
