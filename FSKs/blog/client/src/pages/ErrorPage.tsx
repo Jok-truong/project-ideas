30cf2457fab137e5b1a3fd6be1d8f92c632e1c9a
Huy Truong <o1lkey222k@gmail.com>
2024-05-06 17:02:42 +0700

    blog: done

--- FSKs/blog/client/src/pages/ErrorPage.tsx
+++ FSKs/blog/client/src/pages/ErrorPage.tsx
@@ -1,8 +1,13 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import ComingSoonPage from "./ComingSoonPage";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error, "error");
+
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return <ComingSoonPage />;
+  }
 
   return (
     <div id="error-page">
--- FSKs/blog/client/src/pages/home/HomePage.tsx
+++ FSKs/blog/client/src/pages/home/HomePage.tsx
@@ -1,7 +1,14 @@
 import MainLayout from "../../components/MainLayout";
+import Articles from "./container/Articles";
+import Hero from "./container/Hero";
 
 const HomePage = () => {
-  return <MainLayout>HomePage </MainLayout>;
+  return (
+    <MainLayout>
+      <Hero />
+      <Articles />
+    </MainLayout>
+  );
 };
 
 export default HomePage;
