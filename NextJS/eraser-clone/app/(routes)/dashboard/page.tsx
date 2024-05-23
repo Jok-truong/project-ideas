8cb688333367326c9c080382c2d2039f44a5e86d
Huy Truong <o1lkey222k@gmail.com>
2024-05-23 21:37:08 +0700

    nx/eraser: query and mutation user

--- NextJS/eraser-clone/app/(routes)/dashboard/page.tsx
+++ NextJS/eraser-clone/app/(routes)/dashboard/page.tsx
@@ -1,6 +1,44 @@
+"use client";
+
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Header from "./components/Header";
+import { useConvex, useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useEffect } from "react";
 
 function DashboardPage() {
+  const convex = useConvex();
+  const { user } = useKindeBrowserClient();
+  //const getUser=useQuery(api.user.getUser,{email:user?.email});
+
+  const createUser = useMutation(api.user.createUser);
+
+  const checkUser = async () => {
+    if (user && user.email && user.given_name && user.picture) {
+      const result = await convex.query(api.user.getUser, {
+        email: user.email,
+      });
+
+      if (!result.length) {
+        createUser({
+          name: user.given_name,
+          email: user.email,
+          image: user.picture,
+        }).then((res) => {
+          console.log(res, "res");
+
+          return res;
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      checkUser();
+    }
+  }, [user]);
+
   return (
     <div className="p-8">
       <Header />
