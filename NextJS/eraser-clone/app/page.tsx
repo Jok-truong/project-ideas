4419fea0795c8b6ff87bb67b424d9a4b03bb095b
Huy Truong <o1lkey222k@gmail.com>
2024-05-23 16:57:51 +0700

    nx/eraser: landing screen & logic, register by kinde

--- NextJS/eraser-clone/app/page.tsx
+++ NextJS/eraser-clone/app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
-      <Button className="bg-red-600">Hello</Button>
+      <Header />
+      <Hero />
     </>
   );
 }
