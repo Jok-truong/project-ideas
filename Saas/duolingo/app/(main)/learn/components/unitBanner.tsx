0eabec4f55230541e9b0122a5d0238c8675e8826
Huy Truong <o1lkey222k@gmail.com>
2024-06-01 22:21:30 +0700

    saas/duolingo: fix theme again

--- Saas/duolingo/app/(main)/learn/components/unitBanner.tsx
+++ Saas/duolingo/app/(main)/learn/components/unitBanner.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { NotebookText } from "lucide-react";
 import Link from "next/link";
+import { useMedia } from "react-use";
 
 type Props = {
   title: string;
@@ -8,17 +9,20 @@ type Props = {
 };
 
 function UnitBanner({ title, description }: Props) {
+  const isMobile = useMedia("(max-width: 1024px)");
+  console.log(isMobile, "isMobile");
+
   return (
-    <div className="w-full rounded-xl bg-green-500 p-5 text-white flex items-center justify-between mt-[28px]">
+    <div className="w-full rounded-xl bg-green-500 p-5 flex items-center justify-between mt-[28px]">
       <div className="space-y-2.5">
         <h3 className="text-2xl font-bold">{title}</h3>
         <p className="text-lg">{description}</p>
       </div>
       <Link href="/lesson">
         <Button
-          size="lg"
+          size={isMobile ? "sm" : "lg"}
           variant="secondary"
-          className="hidden xl:flex border-2 border-b-4 active:border-b-2"
+          className="xl:flex border-2 border-b-4 active:border-b-2 text-white"
         >
           <NotebookText className="mr-2" />
           Continue
