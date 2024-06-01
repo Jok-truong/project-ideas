0f3ec4700f398582bf6bb6c5d5793bb9e9e7263a
Huy Truong <o1lkey222k@gmail.com>
2024-06-01 15:13:05 +0700

    saas/duolingo: learderboard page - done

---
+++ Saas/duolingo/app/(main)/leaderboard/page.tsx
@@ -0,0 +1,75 @@
+import { StickyWrapper } from "@/components/StickyWrapper";
+import UserProgress from "@/components/UserProgress";
+import { getUserProgress } from "@/db/queries/user";
+import { redirect } from "next/navigation";
+import { FeedWrapper } from "@/components/FeedWrapper";
+import { getTopTenUsers } from "@/db/queries/leaderBoard";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+const LeaderboardPage = async () => {
+  const userProgressData = await getUserProgress();
+  const leaderboardData = await getTopTenUsers();
+
+  const [userProgress, leaderboard] = await Promise.all([
+    userProgressData,
+    leaderboardData,
+  ]);
+
+  console.log(leaderboard, "leaderboard");
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+  return (
+    <div className="w-full flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+          <Image
+            src="/img/icons/leaderboard.svg"
+            alt="Leaderboard"
+            height={90}
+            width={90}
+          />
+          <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+            Leaderboard
+          </h1>
+          <p className="text-muted-foreground text-center text-lg mb-6">
+            See where you stand amongst your peers in the community.
+          </p>
+          <Separator className="mb-4 h-0.5 rounded-full" />
+          {leaderboard.map((userProgress, index) => (
+            <div
+              key={userProgress.userId}
+              className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+            >
+              <p className="font-bold text-lime-700 mr-4 text-xl">
+                {index + 1}
+              </p>
+              <Avatar className="border bg-green-500 h-12 w-12 ml-3 mr-6">
+                <AvatarImage
+                  className="object-cover"
+                  src={userProgress.userImageSrc}
+                />
+              </Avatar>
+              <p className="font-bold text-neutral-800 flex-1">
+                {userProgress.userName}
+              </p>
+              <p className="text-muted-foreground">{userProgress.points} XP</p>
+            </div>
+          ))}
+        </div>
+      </FeedWrapper>
+    </div>
+  );
+};
+
+export default LeaderboardPage;
