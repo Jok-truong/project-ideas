4d3a617b3c45474e28468513d47901ad413cb6c6
Huy Truong <o1lkey222k@gmail.com>
2024-06-16 22:29:46 +0700

    nuxt/twitter: setup project

---
+++ Nuxt/twitter/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <h1 class="text-3xl font-bold underline">Twitter</h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const darkMode = ref(false);
+</script>
